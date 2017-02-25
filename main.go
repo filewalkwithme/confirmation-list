4ec18c1de79e58a365cc616163172df5e3b25ce2
maiconio <maiconscosta@gmail.com>
2017-02-25 18:32:30 -0300

    Add flag to tell when to read from CSV

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"log"
 	"os"
 	"strings"
@@ -14,38 +15,44 @@ type guest struct {
 }
 
 func main() {
+	var populateDB bool
+	flag.BoolVar(&populateDB, "populate-db", false, "")
+	flag.Parse()
+
 	db, err := initializeDB()
 	if err != nil {
 		panic("")
 	}
 	defer db.Close()
 
-	guestsFile, err := os.Open("guests.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var guests []guest
-
-	guestsScanner := bufio.NewScanner(guestsFile)
-	for guestsScanner.Scan() {
-		line := guestsScanner.Text()
-		fields := strings.Split(line, ";")
-		if len(fields) != 2 {
-			log.Fatalf("Error reading line: %v", line)
-		}
-		guests = append(guests, guest{name: fields[0], email: fields[1], confirmationCode: generateConfirmationCode()})
-	}
-
-	for _, guest := range guests {
-		log.Printf("Name: %v, Email: %v", guest.name, guest.email)
-		err = insertGuest(db, guest)
+	if populateDB {
+		guestsFile, err := os.Open("guests.csv")
 		if err != nil {
 			log.Fatal(err)
 		}
-	}
 
-	if err := guestsScanner.Err(); err != nil {
-		log.Fatalf("reading standard input: %v", err)
+		var guests []guest
+
+		guestsScanner := bufio.NewScanner(guestsFile)
+		for guestsScanner.Scan() {
+			line := guestsScanner.Text()
+			fields := strings.Split(line, ";")
+			if len(fields) != 2 {
+				log.Fatalf("Error reading line: %v", line)
+			}
+			guests = append(guests, guest{name: fields[0], email: fields[1], confirmationCode: generateConfirmationCode()})
+		}
+
+		for _, guest := range guests {
+			log.Printf("Name: %v, Email: %v", guest.name, guest.email)
+			err = insertGuest(db, guest)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if err := guestsScanner.Err(); err != nil {
+			log.Fatalf("reading standard input: %v", err)
+		}
 	}
 }
