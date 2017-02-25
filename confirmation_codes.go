dbabed15e96a6eae619a42f250b4acf813007c0b
maiconio <maiconscosta@gmail.com>
2017-02-25 15:27:08 -0300

    Refactoring confirmation code routines to own file

---
+++ confirmation_codes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func generateConfirmationCode() string {
+	b := make([]rune, 6)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
-import (
-	"math/rand"
-	"time"
-
-	_ "github.com/mattn/go-sqlite3"
-)
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func RandStringRunes(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
-}
-
 func main() {
-	db, err := initializeDB()
-	if err != nil {
-		panic("")
-	}
-	defer db.Close()
+	/*
+		db, err := initializeDB()
+		if err != nil {
+			panic("")
+		}
+		defer db.Close()
+	*/
 }
