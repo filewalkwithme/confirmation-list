1c34c5a1899c9991b91cd4049f2516d9647a480c
maiconio <maiconscosta@gmail.com>
2017-02-25 14:54:52 -0300

    initial algorithm to generate random confirmation codes

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func main() {
+	fmt.Printf("%v\n", RandStringRunes(6))
+}
