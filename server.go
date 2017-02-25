b90d97513d92ff115e8f027a43b17075ecec7ceb
maiconio <maiconscosta@gmail.com>
2017-02-25 19:48:20 -0300

    Receive confirmation request

--- server.go
+++ server.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func initializeWebServer() {
@@ -12,5 +13,23 @@ func initializeWebServer() {
 }
 
 func confirmationServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Confirmed!\n")
+	if strings.ToUpper(req.Method) == "POST" {
+		err := req.ParseForm()
+		if err == nil {
+			nameField, nameOk := req.PostForm["name"]
+			confirmationCodeField, confirmationCodeOk := req.PostForm["confirmation-code"]
+			companionsField, companionsOk := req.PostForm["companions"]
+			if !(nameOk && confirmationCodeOk && companionsOk) {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			name := nameField[0]
+			confirmationCode := confirmationCodeField[0]
+			companions := companionsField[0]
+
+			fmt.Fprintf(w, "%v: %v: %v\n", name, confirmationCode, companions)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusBadRequest)
 }
