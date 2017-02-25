a6fbd4ea91f8a5402e1b6b9e7e773c36baa64a65
maiconio <maiconscosta@gmail.com>
2017-02-25 15:33:20 -0300

    Reading guests from csv file

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 func main() {
 	/*
 		db, err := initializeDB()
@@ -8,4 +14,17 @@ func main() {
 		}
 		defer db.Close()
 	*/
+
+	guestsFile, err := os.Open("guests.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	guestsScanner := bufio.NewScanner(guestsFile)
+	for guestsScanner.Scan() {
+		log.Println(guestsScanner.Text()) // Println will add back the final '\n'
+	}
+	if err := guestsScanner.Err(); err != nil {
+		log.Fatalf("reading standard input: %v", err)
+	}
 }
